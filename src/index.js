f83225df8158958308c8f90b4e4fae23c829c215
Th3Tou7 <elbasiriy@gmail.com>
2020-08-18 16:56:26 +0100

    filling project section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
+//importing scss file
 import "./scss/main.scss";
 
+//import font awesome icons
 import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/brands";
+import "@fortawesome/fontawesome-free/js/regular";
+
+import $ from "jquery";
+
+$(document).ready(function () {
+  $(".link")
+    .not(":last-of-type")
+    .on("click", function () {
+      $("a").removeClass("active");
+      $(".panel").addClass("panel_unactive");
+      $(this).addClass("active");
+      var panel = $(this).attr("id");
+      $("#panel_" + panel).removeClass("panel_unactive");
+    });
+});
